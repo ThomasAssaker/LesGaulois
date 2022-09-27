10f776a07a35d09a5e9a341f0096cb1846ec132c
assaker thomas <sst4392a@ens.fsi>
2022-09-27 19:18:43 +0200

    Create Gaulois.Java

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,17 @@
+package personnages;
+
+public class Gaulois {
+
+	public Gaulois(String nom, int force, int effetPotion) {
+		super();
+		this.nom = nom;
+		this.force = force;
+		this.effetPotion = effetPotion;
+	}
+	private String nom;
+	private int force;
+	private int effetPotion = 1;
+		
+}
+// TODO auto-generated method stub
+
